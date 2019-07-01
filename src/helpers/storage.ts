7d6a320b29c80e9c94a3009b198fd3e544e2901b
Jonathan Lagneaux <lagneaux.j@gmail.com>
2019-07-02 00:12:24 +0200

    init storage into /steps/.storage

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -3,11 +3,11 @@ import storage from 'node-persist';
 export default class StorageHelper {
 
     public static async storeCurrentStepId (currentStep: number) {
-        await storage.init();
+        await storage.init({dir: './steps/.storage/'});
         storage.setItem('currentStep', currentStep);
     }
     public static async getCurrentStepId (): Promise<number> {
-        await storage.init();
+        await storage.init({dir: './steps/.storage/'});
         return storage.getItem('currentStep');
     }
 }
